4f961eb509fa09ef08ade7b5c8b445a8919afae2
xuncharted <evilonlyone@gmail.com>
2022-02-24 08:02:09 +0200

    Uploading

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+    'use strict'
+    // adjust slider height
+    var winH    = $(window).height(),
+        upperH  = $('.upper-bar').innerHeight(),
+        navH    = $('.navbar').innerHeight();
+    $('.slider, .carousel-item', ).height(winH - ( upperH + navH ))
+
+    // Featured Work Shuffle
+    $('.featured-work ul li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        if ($(this).data('class') === 'all') {
+            $('.shuffle-imgs .col-md').css('opacity', 1);
+        } else {
+            $('.shuffle-imgs .col-md').css('opacity', '.0');
+            $($(this).data('class')).parent().css('opacity', 1);
+        }
+    });
+});
